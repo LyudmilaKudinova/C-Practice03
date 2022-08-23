63af253086ac0582a86da9c99ff9113e17fa4add
lyudmila <kudinova@alio-soft.ru>
2022-08-23 13:39:42 +0400

    Найти гипотенузу в 3D

---
+++ C#Pr03Task002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double Distance_3D(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double dist = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
+    return dist;
+}
+double dist1 = Distance_3D(3,6,8,2,1,-7);
+Console.WriteLine("{0:N2}", dist1);
+
+double dist2 = Distance_3D(7,-5,0,1,-1,9);
+Console.WriteLine("{0:N2}", dist2);
+
+//A (3,6,8); B (2,1,-7), -> 15.84
+
+//A (7,-5, 0); B (1,-1,9) -> 11.53
---
+++ C#Pr03Task003/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Write("Введите любое целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Cube_Number( int number)
+{
+    int index = 1;
+    while(index <= number)
+    {
+        Console.Write($"{index*index*index}, ");
+        index++;
+    }
+    return 0;
+}
+
+Cube_Number(number);
